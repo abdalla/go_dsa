75517c1a89857b3452a98b1e8c8dc972177a1031
abdalla <abdalla@msdev.com.br>
2022-10-27 11:43:21 -0300

    bfs - implemented

--- tree/traversal/bfs.go
+++ tree/traversal/bfs.go
@@ -7,6 +7,26 @@ import (
 )
 
 func Levelorder(t *tree.Tree) (traversal []int) {
+	if t == nil {
+		return
+	}
+
+	queue := []*tree.Tree{t}
+
+	for len(queue) > 0 {
+		current := queue[0]
+		traversal = append(traversal, current.Value)
+		queue = queue[1:]
+
+		if current.Left != nil {
+			queue = append(queue, current.Left)
+		}
+
+		if current.Right != nil {
+			queue = append(queue, current.Right)
+		}
+
+	}
 
 	return
 }
