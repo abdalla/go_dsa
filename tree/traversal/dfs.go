cffdbd76b8e886ffceb9078faeda7757e0c51de1
Carlos Abdalla <carlos.abdalla@ifood.com.br>
2022-10-27 09:36:42 -0300

    dfs - implemented

--- tree/traversal/dfs.go
+++ tree/traversal/dfs.go
@@ -6,6 +6,7 @@ import (
 	"github.com/abdalla/go_dsa/tree"
 )
 
+// - ROOT > LEFT > RIGHT
 func preorder(t *tree.Tree) (traversal []int) {
 	if t == nil {
 		return
@@ -18,7 +19,35 @@ func preorder(t *tree.Tree) (traversal []int) {
 	return
 }
 
+// - LEFT > ROOT > RIGHT
+func inorder(t *tree.Tree) (traversal []int) {
+	if t == nil {
+		return
+	}
+
+	traversal = append(traversal, inorder(t.Left)...)
+	traversal = append(traversal, t.Value)
+	traversal = append(traversal, inorder(t.Right)...)
+
+	return
+}
+
+// - LEFT > RIGHT > ROOT
+func postorder(t *tree.Tree) (traversal []int) {
+	if t == nil {
+		return
+	}
+
+	traversal = append(traversal, postorder(t.Left)...)
+	traversal = append(traversal, postorder(t.Right)...)
+	traversal = append(traversal, t.Value)
+
+	return
+}
+
 func main() {
 	t := tree.CreateBinaryTree()
-	fmt.Printf("%v", preorder(t))
+	fmt.Printf("%v \n", preorder(t))
+	fmt.Printf("%v \n", inorder(t))
+	fmt.Printf("%v \n", postorder(t))
 }
