787197d18cb2c362f3a68564f2389e43f1164a79
Carlos Abdalla <carlos.abdalla@ifood.com.br>
2022-11-09 15:00:23 -0300

    .

--- challenges/realworld/xpto.go
+++ challenges/realworld/xpto.go
@@ -1,37 +1,3 @@
-/*
-*
-Given list of folders, print the path of a given folder from root. If there is no path to the root folder then return an empty string. Root level folders will have 0 as id. The structure of Folder is like this:
-
-class Folder {
-
-int id;
-
-List<int> subfolders;
-
-String name;
-
-}
-
-Ex:
-
-list = [Folder(0, [7, 3], “abc”), Folder(0, [], “xyz”), Folder(8, [], “def”), Folder(7, [9], “ijk), Folder(9, [], “lmn”)]
-
-printPath(9) = “abc” -> “ijk” -> “lmn”
-printPath(8) = ""
-
-Clarification: There can be multiple root level folders. All other folders have unique ids.
-
-Note: ç method may be called multiple times. Design your solution taking that into account
-
-//QUESTION:
-// - HOW MANY FOLDERS ?
-// -
-*
-*/
-//    abc .             xyz
-// 	/  \
-// 7 .   3 .   8
-// 9
 package realworld
 
 import (
